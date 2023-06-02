4fecd66b0beccfef6fed3d6c73027e9c5682a5a2
akhilakhi84 <59158825+akhilakhi84@users.noreply.github.com>
2023-06-02 18:24:13 +0530

    Add files via upload

---
+++ install.py
@@ -0,0 +1,12 @@
+import launch
+
+pkgs = [
+        {"fake_useragent": "fake-useragent"},
+        {"PIL": "PIL"},
+        {"requests": "requests"}
+       ]
+
+for pkg in pkgs:
+    key, val = next(iter(pkg.items()))
+    if not launch.is_installed(key):
+        launch.run_pip(f'install {val}', "requirements for ControlNet Model Downloader")
