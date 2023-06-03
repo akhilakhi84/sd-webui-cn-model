2e5d438eebb1e2ca88fc4d2b9bb72253b3be6687
akhilakhi84 <59158825+akhilakhi84@users.noreply.github.com>
2023-06-03 20:55:32 +0530

    Update template_on_tab.py

--- scripts/template_on_tab.py
+++ scripts/template_on_tab.py
@@ -36,9 +36,7 @@ def on_ui_tabs():
                     full_width=True
                 )
             with gr.Column(scale=6):
-                preview_html = gr.TextArea(interactive=False, lines=1, show_label=False, visible=False)
-        with gr.Row():
-            pr = gr.Progress(50)
+                preview_html = gr.TextArea(interactive=False, lines=1, show_label=False, visible=True)
 
         download_model.click(
             fn=get_model_url,
@@ -87,100 +85,70 @@ def get_model_url(model_name):
         msg = "Downloading "+file_name+"..."
         DOWNLOAD_STATUS = msg
         
-        download_file_thread(url, file_name)
-
-        return gr.TextArea.update(value=msg, visible=True)
+        return download_file(url, file_name)
 
 def download_file(url, file_name, pr=gr.Progress(track_tqdm=True)):
+    gr.TextArea.update(value="Downloading "+file_name+"...", visible=True)
+
     if not os.path.exists(MODELFOLDER):
         os.makedirs(MODELFOLDER)
+    
+    new_file = os.path.join(MODELFOLDER, file_name)
+    print(new_file)
 
-    # Maximum number of retries
     max_retries = 5
-
-    # Delay between retries (in seconds)
     retry_delay = 10
 
     while True:
-        # Check if the file has already been partially downloaded
-        if os.path.exists(file_name):
-            # Get the size of the downloaded file
-            downloaded_size = os.path.getsize(file_name)
-
-            # Set the range of the request to start from the current
-            # size of the downloaded file
+        if os.path.exists(new_file):
+            downloaded_size = os.path.getsize(new_file)
             headers = {"Range": f"bytes={downloaded_size}-"}
         else:
             downloaded_size = 0
             headers = {}
 
-        # Split filename from included path
-        tokens = re.split(re.escape('\\'), file_name)
+        tokens = re.split(re.escape('\\'), new_file)
         file_name_display = tokens[-1]
 
-        # Initialize the progress bar
         progress = tqdm(total=1000000000, unit="B", unit_scale=True,
                         desc=f"Downloading {file_name_display}",
                         initial=downloaded_size, leave=False)
 
-        # Open a local file to save the download
-        with open(file_name, "ab") as f:
+        with open(new_file, "ab") as f:
             while True:
                 try:
-                    # Send a GET request to the URL and save the response to the local file
                     response = requests.get(url, headers=headers, stream=True)
-
-                    # Get the total size of the file
                     total_size = int(response.headers.get("Content-Length", 0))
 
-                    # Update the total size of the progress bar if the `Content-Length` header is present
                     if total_size == 0:
                         total_size = downloaded_size
                     progress.total = total_size
 
-                    # Write the response to the local file and update the progress bar
                     for chunk in response.iter_content(chunk_size=1024):
-                        if chunk:  # filter out keep-alive new chunks
+                        if chunk:
                             f.write(chunk)
                             progress.update(len(chunk))
 
-                    downloaded_size = os.path.getsize(file_name)
-                    # Break out of the loop if the download is successful
+                    downloaded_size = os.path.getsize(new_file)
                     break
                 except ConnectionError as e:
-                    # Decrement the number of retries
                     max_retries -= 1
 
-                    # If there are no more retries, raise the exception
                     if max_retries == 0:
                         raise e
 
-                    # Wait for the specified delay before retrying
                     time.sleep(retry_delay)
 
-        # Close the progress bar
         progress.close()
-        downloaded_size = os.path.getsize(file_name)
-        # Check if the download was successful
+        downloaded_size = os.path.getsize(new_file)
+
         if downloaded_size >= total_size:
             DOWNLOAD_STATUS = "{file_name_display} successfully downloaded."
             print(f"{file_name_display} successfully downloaded.")
-            gr.TextArea(value="{file_name_display} successfully downloaded.", visible=True)
-            break
+            return gr.TextArea(value="{file_name_display} successfully downloaded.", visible=True)
         else:
             DOWNLOAD_STATUS = "Error: File download failed. Retrying... {file_name_display}"
             print(f"Error: File download failed. Retrying... {file_name_display}")
-            gr.TextArea(value="Error: File download failed. Retrying... {file_name_display}", visible=True)
-
-
-def download_file_thread(url, file_name):
-    path_to_new_file = os.path.join(MODELFOLDER, file_name)
-    
-    print(path_to_new_file)
-
-    thread = threading.Thread(target=download_file, args=(url, path_to_new_file))
-
-    # Start the thread
-    thread.start()
+            return gr.TextArea(value="Error: File download failed. Retrying... {file_name_display}", visible=True)
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
